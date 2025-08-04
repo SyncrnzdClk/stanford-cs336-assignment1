df98ceb02b461666c06c9c1b87ec134c464fef28
SyncrnzdClk <2383192448@qq.com>
2025-08-04 22:39:16 +0800

    feat: add embedding, rmsnorm module

---
+++ cs336_basics/embedding.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+from torch.nn import Parameter
+from torchtyping import TensorType
+from einops import einsum
+
+class Embedding(nn.Module):
+    def __init__(self, 
+                 num_embeddings : int, 
+                 embedding_dim : int, 
+                 device : torch.device | None = None,
+                 dtype : torch.dtype | None = None
+                 ) -> None:
+        super().__init__()
+        weights = torch.empty(num_embeddings, embedding_dim, device=device, dtype=dtype)
+        nn.init.trunc_normal_(weights, mean=0, std=1, a=-3, b=3)
+        self.weights : TensorType["num_embeddings d_model", float] = Parameter(weights)
+        
+    def forward(self, token_ids : TensorType["batch_size sequence_length", float]) -> TensorType["batch_size d_model", float]:
+        return self.weights[token_ids]
---
+++ cs336_basics/rmsnorm.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn as nn
+from torch.nn import Parameter
+from torchtyping import TensorType
+
+class RMSNorm(nn.Module):
+    def __init__(self, 
+                 d_model : int,
+                 eps : float = 1e-5, 
+                 device : torch.device | None = None,
+                 dtype : torch.dtype | None = None
+                 ) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.eps = torch.tensor(eps).to(device=device, dtype=dtype)
+        self.device = device
+        self.dtype = dtype
+        self.gain_weights = Parameter(torch.ones(d_model, dtype=dtype))
+        
+    def forward(self,
+                x : TensorType["batch_size sequence_length d_model", float]
+                ) -> torch.Tensor:
+        in_dtype = x.dtype
+        x = x.to(torch.float32)
+        
+        
+        rms = torch.sqrt(torch.sum(torch.square(x), dim=-1)/self.d_model + self.eps).unsqueeze(-1)
+        rms_norm = self.gain_weights * x / rms
+        
+        return rms_norm.to(in_dtype)
